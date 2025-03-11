f46940514d157e13c41adfda323b6afc60822da9
Anne Epstein <4fnutryvzp@snkmail.com>
2025-03-11 19:28:39 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create Web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route
+app.get('/comments', (req, res) => {
+  res.send('Comments page');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
